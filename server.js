cf8dae3820e474733fb85371a3f85a9aaa859b4a
Hassan Ahmed Khan <technicaladam0@gmail.com>
2025-07-09 14:37:17 +0500

    server update

--- server.js
+++ server.js
@@ -31,14 +31,6 @@ app.use((req, res, next) => {
 // Baaki sab middleware/routes yahan se shuru ho
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
 
   app.get("/", (req, res) => {
     res.send("Server is running");
@@ -63,7 +55,23 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Error in Server" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is runnig on ${PORT}`);
-});
+
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => { 
+    console.log("MongoDB connected");
+
+      const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is runnig on ${PORT}`);
+    });
+  })
+  .catch((err) => 
+    console.log("MongoDB connection error:", err)
+);
+
